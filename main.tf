0f2b3272b331a05957a49cbe0407656159ad6086
shivramsingh13 <shivramsingh13@gmail.com>
2023-05-13 10:01:08 +0530

    new resource added

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ resource "azurerm_resource_group" "rg-2" {
     location = "west us"
 }
 
+resource "azurerm_virtual_network" "vnet-1" {
+    name = "vnet"
+    resource_group_name = azurerm_resource_group.rg-2.name
+    location = azurerm_resource_group.rg-2.location
+    address_space = [ "10.0.0.0/16" ]
+
+  
+}
