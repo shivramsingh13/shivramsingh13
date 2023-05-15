f4a18f055298a580e434e651a4de2ed70e2e9001
shivramsingh13 <91517543+shivramsingh13@users.noreply.github.com>
2023-05-15 08:44:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,9 @@ provider "azurerm" {
     
 }
 
-
-resource "azurerm_resource_group" "rg-3" {
-    name = "rg-3"
-    location = "west us"
-}
-
-resource "azurerm_virtual_network" "vnet-1" {
-    name = "vnet"
-    resource_group_name = azurerm_resource_group.rg-3.name
-    location = azurerm_resource_group.rg-3.location
-    address_space = [ "10.0.0.0/16" ]
-
-  
+module "terraform-azurerm-rg" {
+    source = "git::https://github.com/shivramsingh13/terraform-azurerm-rg.git"
+    
+    rg_name = "rg-1"
+    location = "east us"
 }
