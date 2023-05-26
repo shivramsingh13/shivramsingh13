52aaebe17b6d523f21d15ee0048aeaeaa8689aca
shivramsingh13 <shivramsingh13@gmail.com>
2023-05-26 10:57:46 +0530

    modified

--- main.tf
+++ main.tf
@@ -3,17 +3,7 @@ provider "azurerm" {
     
 }
 
-module "terraform-azurerm-VM" {
-    source = "git::https://github.com/shivramsingh13/terraform_azurerm_VM.git"
+module "terraform-applicationgetway" {
+    source = "git::https://github.com/shivramsingh13/terraform-applicationgetway.git"
     
-    rg_name = "rg-1"
-    location = "east us"
-    vm_name = "vm-1"
-    vnet_name = "vnet-1"
-    subnet_name = "subnet-1"
-    public_name = "public_ip"
-    nic_name = "nic-1"
-    os_disk_name = "os_disk_name-1"
-    data_disk_name = "data_disk_name-1"
-
 }
